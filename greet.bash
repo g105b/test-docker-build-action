f2cda82b7b1a5ee2204a99d02fc3218a5353c16e
Greg Bowler <greg.bowler@g105b.com>
2020-12-08 21:24:57 +0000

    Update reference in greet script

--- greet.bash
+++ greet.bash
@@ -2,5 +2,5 @@
 set -e
 ACTION_NAME_NO_SPACES="${ACTION_NAME// /_}"
 ACTION_NAME_NO_SPACES="${ACTION_NAME_NO_SPACES,,}"
-docker_tag="docker.pkg.github.com/$GITHUB_REPO/example-docker-image:php${ACTION_PHP_VERSION}-${ACTION_NAME_NO_SPACES}"
+docker_tag="docker.pkg.github.com/$GITHUB_REPOSITORY/example-docker-image:php${ACTION_PHP_VERSION}-${ACTION_NAME_NO_SPACES}"
 docker run --rm "$docker_tag"
