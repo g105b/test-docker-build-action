63d8f10cb67cbfbcc3b0d5481d985c2583d630e4
Greg Bowler <greg.bowler@g105b.com>
2020-12-08 18:33:39 +0000

    Pass repo and user variables

--- docker-build.bash
+++ docker-build.bash
@@ -3,7 +3,6 @@ set -e
 docker pull -q "php:$ACTION_PHP_VERSION"
 dockerfile="FROM php:$ACTION_PHP_VERSION"
 
-GITHUB_USER=$(curl -sSL -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user | jq -r .login)
 echo "${GITHUB_TOKEN}" | docker login docker.pkg.github.com -u "${GITHUB_USER}" --password-stdin
 
 dockerfile="${dockerfile}
@@ -15,7 +14,7 @@ CMD php /app/greeter.php
 ACTION_NAME_NO_SPACES="${ACTION_NAME// /_}"
 ACTION_NAME_NO_SPACES="${ACTION_NAME_NO_SPACES,,}"
 # Tag the image with the name we've added, so it can be cached per-name.
-docker_tag="docker.pkg.github.com/g105b/test-docker-build-action/example-docker-image:${ACTION_PHP_VERSION}-${ACTION_NAME_NO_SPACES}"
+docker_tag="docker.pkg.github.com/$GITHUB_REPO/example-docker-image:${ACTION_PHP_VERSION}-${ACTION_NAME_NO_SPACES}"
 docker pull "$docker_tag" || echo "Remote tag does not exist yet"
 
 # Build the custom image and attempt to push it.
