70daee7e400f75da4002bbe6284c99f07c372c16
Greg Bowler <greg.bowler@g105b.com>
2020-12-08 13:35:03 +0000

    Build docker image

--- docker-build.bash
+++ docker-build.bash
@@ -1,1 +1,18 @@
 #!/bin/bash
+set -e
+docker pull -q "php:$ACTION_PHP_VERSION"
+dockerfile="FROM php:$ACTION_PHP_VERSION"
+
+# Create the greeting as a file, so it can be added to the custom Docker image.
+touch name.txt
+echo "$ACTION_NAME" > name.txt
+dockerfile="${dockerfile}
+ADD name.txt /tmp/name.txt"
+
+# Tag the image with the name we've added, so it can be cached per-name.
+docker_tag="ghcr.io/g105b/test-docker-build-action:${ACTION_NAME}"
+docker pull "$docker_tag" || echo "Remote tag does not exist"
+
+# Build the custom image and attempt to push it.
+echo "$dockerfile" | docker build --tag "$docker_tag" -
+docker push "$docker_tag"
