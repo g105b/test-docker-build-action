2ce12173026d3edc4413f6dc3c9a83603f46aaaf
Greg Bowler <greg.bowler@g105b.com>
2020-12-08 16:09:32 +0000

    Better wording

--- docker-build.bash
+++ docker-build.bash
@@ -11,7 +11,7 @@ ADD name.txt /tmp/name.txt"
 
 # Tag the image with the name we've added, so it can be cached per-name.
 docker_tag="ghcr.io/g105b/test-docker-build-action:${ACTION_NAME}"
-docker pull "$docker_tag" || echo "Remote tag does not exist"
+docker pull "$docker_tag" || echo "Remote tag does not exist yet"
 
 # Build the custom image and attempt to push it.
 echo "$dockerfile" | docker build --tag "$docker_tag" -
